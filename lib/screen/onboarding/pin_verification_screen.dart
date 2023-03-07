c5b5e87de250ad9f845b647a1457d96a1fe4e690
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-07 21:50:17 +0600

    adding file

---
+++ lib/screen/onboarding/pin_verification_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PinVerificationScreen extends StatefulWidget {
+  const PinVerificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PinVerificationScreen> createState() => _PinVerificationScreenState();
+}
+
+class _PinVerificationScreenState extends State<PinVerificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
