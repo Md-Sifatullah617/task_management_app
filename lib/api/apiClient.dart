34e486f081b83aed1f351e32f8fb0001d02f5d45
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-09 16:52:11 +0600

    done

---
+++ lib/api/apiClient.dart
@@ -0,0 +1,88 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:task_management_app/style/style.dart';
+
+var baseURL="https://task.teamrabbil.com/api/v1";
+var requestHeader={"Content-Type":"application/json"};
+
+
+Future<bool> loginRequest(formValues) async {
+  var uRL=Uri.parse("$baseURL/login");
+  var postBody=json.encode(formValues);
+
+  var response=await http.post(uRL, headers: requestHeader, body: postBody);
+  var resultCode=response.statusCode;
+  var resultBody=json.decode(response.body);
+  if(resultCode==200 && resultBody['status']=='success'){
+    successToast("Request Success");
+    return true;
+  }else{
+    errorToast("Request fail! try again");
+    return false;
+  }
+}
+
+Future<bool> registrationRequest(formValues) async {
+  var uRL=Uri.parse("$baseURL/registration");
+  var postBody=json.encode(formValues);
+
+  var response=await http.post(uRL, headers: requestHeader, body: postBody);
+  var resultCode=response.statusCode;
+  var resultBody=json.decode(response.body);
+  if(resultCode==200 && resultBody['status']=='success'){
+    successToast("Request Success");
+    return true;
+  }else{
+    errorToast("Request fail! try again");
+    return false;
+  }
+}
+
+Future<bool> verifyEmailRequest(email) async {
+  var uRL=Uri.parse("$baseURL/RecoverVerifyEmail/$email");
+
+  var response=await http.get(uRL, headers: requestHeader);
+  var resultCode=response.statusCode;
+  var resultBody=json.decode(response.body);
+  if(resultCode==200 && resultBody['status']=='success'){
+    successToast("Request Success");
+    return true;
+  }else{
+    errorToast("Request fail! try again");
+    return false;
+  }
+}
+
+Future<bool> verifyOTPRequest(email,OTP) async {
+  var uRL=Uri.parse("$baseURL/RecoverVerifyEmail/$email/$OTP");
+
+  var response=await http.get(uRL, headers: requestHeader);
+  var resultCode=response.statusCode;
+  var resultBody=json.decode(response.body);
+  if(resultCode==200 && resultBody['status']=='success'){
+    successToast("Request Success");
+    return true;
+  }else{
+    errorToast("Request fail! try again");
+    return false;
+  }
+}
+
+
+Future<bool> setPwdRequest(formValues) async {
+  var uRL=Uri.parse("$baseURL/RecoverVerifyEmail/RecoverResetPass");
+  var postBody=json.encode(formValues);
+  var response=await http.post(uRL, headers: requestHeader, body: postBody);
+  var resultCode=response.statusCode;
+  var resultBody=json.decode(response.body);
+  if(resultCode==200 && resultBody['status']=='success'){
+    successToast("Request Success");
+    return true;
+  }else{
+    errorToast("Request fail! try again");
+    return false;
+  }
+}
+
+
