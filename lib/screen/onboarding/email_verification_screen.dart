34b1a3efbabf0819b682f3ea4ab808158490019f
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-10 11:38:16 +0600

    email verfication page done

--- lib/screen/onboarding/email_verification_screen.dart
+++ lib/screen/onboarding/email_verification_screen.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../../style/style.dart';
+
 class EmailVerificationScreen extends StatefulWidget {
   const EmailVerificationScreen({Key? key}) : super(key: key);
 
   @override
-  State<EmailVerificationScreen> createState() => _EmailVerificationScreenState();
+  State<EmailVerificationScreen> createState() =>
+      _EmailVerificationScreenState();
 }
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          screenBackground(context),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Your Email Address",
+                  style: head1Text(colorDarkBlue),
+                ),
+                const SizedBox(
+                  height: 1,
+                ),
+                Text(
+                  "A 6 digit verification pin will send to your email address",
+                  style: head6Text(colorLightGray),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: appInputDecoration("Email Address"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: appButtonStyle(),
+                    child: successButtonChild("Next")),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
