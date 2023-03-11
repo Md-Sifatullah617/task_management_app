9697d0ccfcbef0080fb4e7125e22d959274dcc84
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-12 03:06:51 +0600

    gf

--- lib/Component/cancle_task_list.dart
+++ lib/Component/cancle_task_list.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CancleTaskScreen extends StatelessWidget {
+class CancleTaskScreen extends StatefulWidget {
   const CancleTaskScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CancleTaskScreen> createState() => _CancleTaskScreenState();
+}
+
+class _CancleTaskScreenState extends State<CancleTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
