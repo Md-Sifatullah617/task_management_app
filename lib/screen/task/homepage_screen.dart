ece0a1ed9d73d5cca3bafba1d1107b05789f6447
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-12 03:56:29 +0600

    bx

--- lib/screen/task/homepage_screen.dart
+++ lib/screen/task/homepage_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_management_app/Component/bottom_app_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -8,17 +9,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int bottomTabIndex = 0;
+  onTappedIndexChange(int index) {
+    setState(() {
+      bottomTabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Task Management", style: TextStyle(fontFamily: 'poppins', fontWeight: FontWeight.bold),),
+        title: const Text(
+          "Task Management",
+          style: TextStyle(fontFamily: 'poppins', fontWeight: FontWeight.bold),
+        ),
       ),
       body: Column(
-        children: const [
-
-        ],
+        children: const [],
       ),
+      bottomNavigationBar: appBottomNav(bottomTabIndex, onTappedIndexChange),
     );
   }
 }
